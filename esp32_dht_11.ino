e51877f45ff9220270a38065af26bc1155b15515
SURFACE\oleksii.mykhalchuk <ale.m100888@gmail.com>
2022-11-21 10:03:40 +0100

    temperature sensore hdt11 setup and tested

--- esp32_dht_11.ino
+++ esp32_dht_11.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHTesp.h>
 
+DHTesp dht;
+
+#define TEMP_PIN 13
+uint32_t lastTime = 0;
+uint32_t timerDelay = 5000;
+
+
+void setup() {
+  Serial.begin(9600);
+  dht.setup(TEMP_PIN, DHTesp::DHT11);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if ((millis() - lastTime) > timerDelay) {
+    Serial.println("Temperatura = ");
+    Serial.print(dht.getTemperature());
+    Serial.println("\n");
+    lastTime = millis();
+  }
 
 }
