e0745d021e06830437cb4a6edff380b39adfe0af
SURFACE\oleksii.mykhalchuk <ale.m100888@gmail.com>
2022-11-23 16:05:03 +0100

    BLE configuration peripheral device

--- esp32_dht_11.ino
+++ esp32_dht_11.ino
@@ -14,9 +14,43 @@ double offset = 2.0;
 #define PERIPHERAL_NAME "ESP32 BLE"
 #define SERVICE_UUID "da30e919-38b1-469e-9081-da9f59c04c34"
 #define CHARACTERISTIC_UUID "f8abccc0-488f-4747-8dd2-808a4c70bfc3"
+#define CHARACTERISTIC_INPUT_UUID "04b250d7-c16b-4905-bbb3-65b597685dae"
+#define CHARACTERISTIC_OUTPUT_UUID "ad0d60a7-6770-4383-a879-5cd77d75ec26"
+
+static uint8_t outputData[1];
+
+BLECharacteristic *pOutputChar;
+
+class ServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    Serial.println("BLE Client connected");
+  }
+  void onDisconnect(BLEServer *pServer) {
+    BLEDevice::startAdvertising();
+    Serial.println("BLE Client Disconnected");
+  }
+};
 
 class InputReceivedCallbacks: public BLECharacteristicCallbacks {
-  
+  void onWrite(BLECharacteristic *pCharWriteState) {
+    uint8_t *inputValues = pCharWriteState->getData();
+    switch(inputValues[2]) {
+      case 0x00: // add
+      outputData[0] = inputValues[0] + inputValues[1];
+      break;
+      case 0x01: // subtract
+      outputData[0] = inputValues[0] - inputValues[1];
+      break;
+      default: // multiply
+      outputData[0] = inputValues[0] * inputValues[1];
+      break;
+    }
+    pOutputChar->setValue((uint8_t *)outputData, 1);
+    pOutputChar->notify();
+    Serial.println("input received ");
+    Serial.print(*inputValues);
+    Serial.println();
+  }
 };
 
 void setup() {
@@ -30,15 +64,35 @@ void setup() {
   BLECharacteristic *pInputChar = pService->createCharacteristic(
     CHARACTERISTIC_UUID, 
     BLECharacteristic::PROPERTY_WRITE_NR | BLECharacteristic::PROPERTY_WRITE);
+
+  pOutputChar = pService->createCharacteristic(CHARACTERISTIC_OUTPUT_UUID,
+  BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+
+  pInputChar->setCallbacks(new InputReceivedCallbacks());
+  pServer->setCallbacks(new ServerCallbacks());
+
+  outputData[0] = 0x00;
+  pOutputChar->setValue((uint8_t *)outputData, 1);
+
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  BLEDevice::startAdvertising();
 }
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
     TempAndHumidity data = dht.getTempAndHumidity();
     temp = data.temperature;
+    float current = temp - offset;
     Serial.println("Temperatura = ");
-    Serial.print(temp - offset);
+    Serial.print(current);
     Serial.println("");
+    outputData[0] = current;
+    pOutputChar->setValue((uint8_t*)outputData, 1);
+    pOutputChar->notify();
     
     lastTime = millis();
   }
